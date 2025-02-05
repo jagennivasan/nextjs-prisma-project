1be7bea2aa96df98d251dfca4a61dc8e74dc2ba2
jagennivasan <professorjagan18@gmail.com>
2025-02-05 22:15:23 +0530

    changes for register route

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -4,7 +4,7 @@ import { redirect } from "next/navigation";
 import { authOptions } from "@/lib/auth";
 export default async function Register(){
   const session = await getServerSession(authOptions);
-  if(session) redirect("/dashboard")
+  if(session) redirect("/todos")
     return(
       <RegisterForm/>
     )
